d27eab6876159099380d12a536618c26d183ad23
swiftbook <swiftbook@macbook-pro-john.local>
2023-10-01 21:19:52 +0300

    Initial Commit

---
+++ ColorizedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorizedApp
+//
+//  Created by swiftbook on 01.10.2023.
+//  Copyright © 2023 swiftbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
